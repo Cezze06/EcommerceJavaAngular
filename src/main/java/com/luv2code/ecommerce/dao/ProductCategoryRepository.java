0324e18b4a69b1f2361f84c13b570e050e53f5cf
Carlos J <cejimenez@unal.edu.co>
2021-12-28 11:35:30 -0500

    CrossOriginAdded

--- src/main/java/com/luv2code/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/luv2code/ecommerce/dao/ProductCategoryRepository.java
@@ -3,9 +3,12 @@ package com.luv2code.ecommerce.dao;
 import com.luv2code.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-                                                //Name of JSOn entry      Path actual
+//Name of JSOn entry      Path actual
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")   //Custom options, Este se usa para acceder desde URL
-                                                                                                //localhost:8080/api/product-categoroy
+                                                                                                //localhost:8080/api/product-category
+
+@CrossOrigin("http://localhost:4200")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
