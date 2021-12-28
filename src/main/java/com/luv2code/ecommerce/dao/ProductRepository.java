45c475831b910f83eb68a87eba98190ccf78d9e6
Carlos J <cejimenez@unal.edu.co>
2021-12-28 16:03:55 -0500

    CrossOriginAdded v0.1

--- src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 //import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//Accessing through URL
 //@RepositoryRestResource(collectionResourceRel = "product", path = "product")
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
